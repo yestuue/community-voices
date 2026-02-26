31bddca80b1020b30e9f364ea0edce6c80bee23d
Samuel Opeyemi <growthprofesors@gmail.com>
2026-02-26 14:58:17 +0100

    added vercel analytics component

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+
 // 1. Import the Analytics component
 import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    {/* 2. Place it here to start tracking */}
+    {/* 2. Place it here to start tracking visitors */}
     <Analytics />
   </React.StrictMode>,
 );
